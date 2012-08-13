d113c4ac35793f6b948a490200c84bbd3950b930
Jan Potoms <user@server>
2012-08-13 10:41:57 +0200

    fix bootsrap

--- app/js/app.js
+++ app/js/app.js
@@ -5,6 +5,10 @@
 angular.module('anylogr', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('', {templateUrl: 'partials/home.html',   controller: HomeCtrl}).
-      otherwise({redirectTo: ''});
+      when('/data', {templateUrl: 'partials/data.html',   controller: DataCtrl}).
+      when('/data/:path1', {templateUrl: 'partials/data.html',   controller: DataCtrl}).
+      when('/data/:path1/:path2', {templateUrl: 'partials/data.html',   controller: DataCtrl}).      
+      when('/data/:path1/:path2/:path3', {templateUrl: 'partials/data.html',   controller: DataCtrl}).
+      when('/data/:path1/:path2/:path3/:path4', {templateUrl: 'partials/data.html',   controller: DataCtrl}).
+      otherwise({redirectTo: '/data'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,5 +2,17 @@
 
 /* Controllers */
 
-function HomeCtrl($scope) {
+function DataCtrl($scope, $routeParams) {
+    var buildUrl = '/data';
+    $scope.path = [{part: 'data', url: buildUrl}];
+    
+    for (var i = 1; i <= 4; i++) {
+        var part = $routeParams['path' + i];
+        if (part != undefined) {
+            buildUrl += '/' + part;
+            $scope.path.push({part: part, url: buildUrl});
+        } else {
+            break;
+        }
+    }
 }
